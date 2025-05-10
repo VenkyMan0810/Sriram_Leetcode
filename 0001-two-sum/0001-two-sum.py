9e49e48419378fdf6521d7d601e63d9466df4535
Sriram Venkatesh <145090945+VenkyMan0810@users.noreply.github.com>
2025-05-09 22:55:17 -0400

    Time: 4 ms (41.22%), Space: 19 MB (24.58%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,20 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        ind = []
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i!=j and nums[i] + nums[j] == target:
-                    return [i,j]
+        # ind = []
+        # for i in range(len(nums)):
+        #     for j in range(len(nums)):
+        #         if i!=j and nums[i] + nums[j] == target:
+        #             return [i,j]
+
+        numMap = {}
+        n = len(nums)
+
+        for i in range(n):
+            numMap[nums[i]] = i
+
+        for i in range(n):
+            comp = target - nums[i]
+            if comp in numMap and numMap[comp] != i:
+                return [i, numMap[comp]]
+
+        return []
