86e53e6a0503d504c97882ebbce710699ff8a0aa
Sriram Venkatesh <145090945+VenkyMan0810@users.noreply.github.com>
2025-05-23 11:50:55 -0400

    Time: 580 ms (42.49%), Space: 0B (100%) - LeetHub

---
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT
+employee_id,
+CASE
+    WHEN COUNT(department_id) = 1 THEN MAX(department_id)
+    ELSE MAX(CASE WHEN primary_flag = 'Y' THEN department_id END)
+    END AS department_id
+FROM Employee 
+GROUP BY employee_id;
