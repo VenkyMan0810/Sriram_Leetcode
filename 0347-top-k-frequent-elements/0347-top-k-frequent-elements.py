8006fc3d6288c1497ec9c1b0560f23049f0fd163
Sriram Venkatesh <145090945+VenkyMan0810@users.noreply.github.com>
2025-06-25 13:24:51 -0400

    Time: 4 ms (69.87%), Space: 21.3 MB (61.6%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,17 +1,27 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        freq = [[] for i in range(len(nums) + 1)]
+        # count = {}
+        # freq = [[] for i in range(len(nums) + 1)]
 
-        for i in nums:
-            count[i] = 1 + count.get(i, 0)
+        # for i in nums:
+        #     count[i] = 1 + count.get(i, 0)
 
-        for n, c in count.items():
-            freq[c].append(n)
+        # for n, c in count.items():
+        #     freq[c].append(n)
 
-        res = []
-        for i in range(len(freq)-1, 0 , -1):
-            for n in freq[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
+        # res = []
+        # for i in range(len(freq)-1, 0 , -1):
+        #     for n in freq[i]:
+        #         res.append(n)
+        #         if len(res) == k:
+        #             return res
+
+        heap = []
+        counter = Counter(nums)
+
+        for i, j in counter.items():
+            heapq.heappush(heap, (j, i))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return [i for j, i in heap]
