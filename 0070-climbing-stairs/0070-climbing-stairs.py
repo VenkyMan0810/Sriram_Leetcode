1cb771c0471abfef1d713c87da128fae5cd15d6a
Sriram Venkatesh <145090945+VenkyMan0810@users.noreply.github.com>
2025-07-05 19:06:17 -0400

    Time: 0 ms (100%), Space: 17.5 MB (95.64%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,10 +4,16 @@ class Solution:
             return 1
         if n == 2:
             return 2
-        res = [0] * (n+1)
-        res [1], res[2] = 1, 2
+        # res = [0] * (n+1)
+        # res [1], res[2] = 1, 2
         
-        for i in range(3, n+1):
-            res[i] = res[i-1] + res[i-2]
+        # for i in range(3, n+1):
+        #     res[i] = res[i-1] + res[i-2]
 
-        return res[n]
+        # return res[n]
+        prev, cur = 1, 2
+
+        for i in range(3, n+1):
+            prev, cur = cur, cur+prev
+
+        return cur
